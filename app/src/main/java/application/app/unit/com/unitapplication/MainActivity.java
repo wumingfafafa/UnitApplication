e1749e1cb6da8f2d898c244360ba230b6794f685
pluto <pluto@ixiaopu.com>
2016-09-18 14:45:59 +0800

    moudel形式添加工具库

--- app/src/main/java/application/app/unit/com/unitapplication/MainActivity.java
+++ app/src/main/java/application/app/unit/com/unitapplication/MainActivity.java
@@ -3,18 +3,30 @@ package application.app.unit.com.unitapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.blankj.utilcode.utils.DeviceUtils;
-import com.blankj.utilcode.utils.ShellUtils;
-
-import application.app.unit.com.unitapplication.log.LogUtil;
+import com.blankj.utilcode.utils.ScreenUtils;
+import com.github.lazylibrary.util.DoubleClickExitDetector;
 
 public class MainActivity extends AppCompatActivity {
-
+    DoubleClickExitDetector doubleClickExitDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ScreenUtils.hideStatusBar(this);
+        ScreenUtils.hideNotificationBar(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LogUtil.e("ShellUtils.isRoot()"+ShellUtils.isRoot(),true);
-        LogUtil.e("DeviceUtils.getMacAddress()"+ DeviceUtils.getMacAddress(),true);
+        doubleClickExitDetector = new DoubleClickExitDetector(this);
+//        LogUtil.e("ShellUtils.isRoot()"+ShellUtils.isRoot(),true);
+//        LogUtil.e("DeviceUtils.getMacAddress()"+ DeviceUtils.getMacAddress(),true);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+
+        doubleClickExitDetector.setEffectiveIntervalTime(2000);
+        if(doubleClickExitDetector.click()){
+            super.onBackPressed();
+            finish();
+        }
     }
 }
